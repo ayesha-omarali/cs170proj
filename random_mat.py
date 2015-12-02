e7656b920109346167433b29ee497ab8dc7f3169
Apollo Jain <apollojain@gmail.com>
2015-12-02 00:25:41 -0800

    Create random_mat.py

---
+++ random_mat.py
@@ -0,0 +1,24 @@
+import random
+def random_num():
+	i = random.randint(1, 4)
+	if i == 4:
+		return "0 "
+	else:
+		return "1 "
+def random_mat(n, numba):
+	ret = str(n) + "\n"
+	for i in range(0, n):
+		for j in range(0, n):
+			if i == j:
+				ret += "0 "
+			else:
+				ret += random_num()
+		ret += "\n"
+	text_file = open("matrix" + str(numba) + ".txt", "w")
+	text_file.write(ret)
+	text_file.close()
+	return ret
+
+random_mat(100, 1)
+random_mat(100, 2)
+random_mat(100, 3)
