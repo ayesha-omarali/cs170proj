08771a0932e8d3aea7e51cfdd8e28f9f8ddca13d
apollojain@gmail.com <apollojain@gmail.com>
2015-12-03 00:02:08 -0800

    belman ford

--- functions.py
+++ functions.py
@@ -63,5 +63,46 @@ def topological_sort(matrix):
 		print(top_sort)
 		return top_sort
 
+# Step 1: For each node prepare the destination and predecessor
+def initialize(graph, source):
+    d = {} # Stands for destination
+    p = {} # Stands for predecessor
+    for node in range(0, len(graph)):
+        d[node] = float('Inf') # We start admiting that the rest of nodes are very very far
+        p[node] = None
+    d[source] = 0 # For the source we know how to reach
+    return d, p
+
+def relax(node, neighbour, graph, d, p):
+    # If the distance between the node and the neighbour is lower than the one I have now
+    if graph[node][neighbour] != 0 and d[neighbour] > d[node] + graph[node][neighbour]:
+        # Record this lower distance
+        d[neighbour] = d[node] + graph[node][neighbour]
+        p[neighbour] = node
+
+def bellman_ford(graph, source):
+    d, p = initialize(graph, source)
+    for i in range(len(graph)-1): #Run this until is converges
+        for u in range(0, len(graph)):
+            for v in range(0, len(graph[u])): #For each neighbour of u
+                relax(u, v, graph, d, p) #Lets relax it
+
+    # Step 3: check for negative-weight cycles
+    for u in range(0, len(graph)):
+        for v in range(0, len(graph[u])): #For each neighbour of u
+            assert d[v] <= d[u] + graph[u][v] or graph[u][v] == 0
+
+    return d, p
+
+
+
 topological_sort([[0, 0, 1], [0, 0, 0], [0, 0, 0]])
+graph = [
+	[0, 1, 1, 1, 0],
+	[0, 0, 1, 0, 0],
+	[0, 0, 0, 1, 0],
+	[0, 0, 0, 0, 1],
+	[1, 0, 0, 0, 0]
+]
+print(bellman_ford(graph, 0))
 							
