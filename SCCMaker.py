ad4d12bd47e02ae62e9f9f485650778cc24b613b
Sam Holladay <sam.holladay@berkeley.edu>
2015-12-05 13:05:08 -0800

    Working Kosaraju

--- SCCMaker.py
+++ SCCMaker.py
@@ -6,81 +6,14 @@ class SCCMaker:
 
     # returns a new matrix with all pointers pointing from j to i
     # instead of i to j
-<<<<<<< HEAD
     def flippedGraph(self):
-    	toReturn = [[0 for j in self.Matrix[i]] for i in self.Matrix] # Matrix of zeroes
-=======
 
         toReturn = [[0 for j in i] for i in self.Matrix] # Matrix of zeroes
->>>>>>> refs/remotes/origin/master
 
         for i in range(len(self.Matrix)):
             for j in range(len(self.Matrix[i])):
                 if self.Matrix[i][j] == 1: 
                     toReturn[j][i] = 1
-
-<<<<<<< HEAD
-    	return toReturn
-    
-    #legal Move from i to j 
-    def legalMove(self, i, j): 
-    	if self.Matrix[i][j] == 1: 
-    		return True
-
-    	return False
-
-   	def getAllLegalMoves(self, i):
-   		possibleMoves = []
-   		for j in range(len(self.Matrix[i])):
-
-   			if self.legalMove(i, j):
-   				possibleMoves.append(j)
-
-   		return possibleMoves
-
-   	def makePostOrder(self):
-   		nodesToVisit = set([i for i in range(100)])
-   		nodesVisited = set([])
-   		postOrder = []
-
-   		def DFS(v):
-   			nonlocal nodesVisited
-   			nonlocal nodesToVisit
-   			nonlocal postOrder
-
-   			nodesToVisit.remove(v)
-   			nodesVisited.add(v)
-
-   			for move in getAllLegalMoves(v):
-   				if move not in nodesVisited:
-   					DFS(move)
-
-   			postOrder = postOrder.insert(0,v)
-
-   		while nodesToVisit:
-   			v = nodesToVisit.pop()
-   			DFS(v)
-
-   		return postOrder
-
-   	def Kosarajus(self): 
-   		postOrder = self.makePostOrder()
-   		flipped = SCCMaker(self.flippedGraph())
-
-
-      ##SHOULD IMPLEMENT DFS FROM FUNCTIONS ALREADY WRITTEN
-   		def DFS(v, lst):
-   			nonlocal postOrder
-
-   			postOrder.remove(v)
-   			for move in flipped.getAllLegalMoves(v):
-
-   				if move in postOrder:
-   					lst.append(DFS(move, lst))
-
-   			return lst
-
-=======
         return toReturn
 
     def legalMove(self, i, j): 
@@ -124,16 +57,25 @@ class SCCMaker:
     def Kosarajus(self): 
         postOrder = self.makePostOrder()
         flipped = SCCMaker(self.flippedGraph())
+        postOrder = list(reversed(postOrder))
         
         def DFS(v, lst):
             nonlocal postOrder
+            #print(postOrder)
+            #print(v)
             
             postOrder.remove(v)
+            lst.append(v)
+            #print(flipped.getAllLegalMoves(v))
             for move in flipped.getAllLegalMoves(v):
                 if move in postOrder:
-                    lst.append(DFS(move, lst))
+                    DFS(move, lst)
             return lst
-            
-         return DFS()
+        sccList = []   
+        for v in postOrder:
+            dfslist = DFS(v, [])
+            sccList.append(dfslist)
+        print(len(sccList))
+        print(sccList)
+        return sccList
         
->>>>>>> refs/remotes/origin/master
--- solve.py
+++ solve.py
@@ -4,18 +4,19 @@ def solve(file):
     if file.endswith('.in'):
         mat = utils.read_matrix(file)
         sccmaker = SCCMaker.SCCMaker(mat)
-        sccmaker.Kosarajus()
-        sccDag = sccmaker.Matrix
+        sccDag = sccmaker.Kosarajus()
         sorted = functions.topological_sort(sccDag)
         solutionlist = []
+        print(sorted)
         for s in sorted:
             ordering = functions.efficient_cycle_analysis(s, mat)
+            print(ordering)
             solutionlist.extend(ordering)
         filenum = file[:-3]
         solname='solcheck'
         sol = open(solname, 'wb')
         stringsol = ' '.join(map(str,solutionlist))
-        sol.write(stringsol)
+        sol.write(input(stringsol))
         sol.close()
         scorestring = scorer_single.processTest(file, solname)
         print(scorestring)
